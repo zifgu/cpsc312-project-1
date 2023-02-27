fa314cb8be96ffd3421694ed7e2e15b6bfb2e554
huynh02-stu <114965223+huynh02-stu@users.noreply.github.com>
2023-02-26 17:01:21 -0800

    Basic typed IO for project

--- Main.hs
+++ Main.hs
@@ -24,3 +24,28 @@ printImageStats image = do
     putStrLn ("Pixel (0, 100): " ++ show (get image 0 100 0) ++ "," ++ show (get image 0 100 1) ++ "," ++ show (get image 0 100 2))
     putStrLn ("Pixel (60, 20): " ++ show (get image 60 20 0) ++ "," ++ show (get image 60 20 1) ++ "," ++ show (get image 60 20 2))
     putStrLn ("Pixel (100, 100): " ++ show (get image 100 100 0) ++ "," ++ show (get image 100 100 1) ++ "," ++ show (get image 100 100 2))
+
+
+-- Expected user flow: Input string per line for url/pathway which outputs an image.
+-- Users who wish to do this multiple times will need to type go again.
+
+go :: IO (String, String, String, String) 
+go = do
+      putStrLn ("Insert the 1st Image URL or filepath.")
+      firstURL <- getLine
+      putStrLn ("Insert the 2nd Image URL or filepath.")
+      secondURL <- getLine
+      putStrLn ("Insert the filter Image URL or filepath.")
+      filterURL <- getLine
+      putStrLn ("Insert the name of the combined image.")
+      combinedImageName <- getLine
+      putStrLn ("Congratulations on merging your images! To see the result go to runtime-images folder.")
+      putStrLn(" ")
+      putStrLn ("   History:")
+      putStrLn ("       firstURL = " ++ firstURL)
+      putStrLn ("       secondURL = " ++ secondURL)
+      putStrLn ("       filterURL = " ++ filterURL)
+      putStrLn ("       combinedImageName = " ++ combinedImageName)
+       putStrLn(" ")
+      return (firstURL, secondURL, filterURL, combinedImageName)
+      
