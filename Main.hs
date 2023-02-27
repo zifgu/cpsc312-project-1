6066dc856bb07d01c3fc4ebf600445ed3dd9ae63
huynh02-stu <114965223+huynh02-stu@users.noreply.github.com>
2023-02-26 19:30:10 -0800

    IO integration and small error with image reading

--- Main.hs
+++ Main.hs
@@ -1,5 +1,6 @@
 import Image
 import ImageRead
+import Control.Exception
 
 testFromFile :: String -> IO ()
 testFromFile filepath = do
@@ -26,26 +27,40 @@ printImageStats image = do
     putStrLn ("Pixel (100, 100): " ++ show (get image 100 100 0) ++ "," ++ show (get image 100 100 1) ++ "," ++ show (get image 100 100 2))
 
 
+readingImage :: String -> IO ()
+readingImage imagename = do
+    result <- readImageFromFile imagename
+    case result of
+        (Left str) -> readImageFromUrl str
+        (Right img) -> printImageStats img
+    putStrLn ("Read Complete")
+
 -- Expected user flow: Input string per line for url/pathway which outputs an image.
 -- Users who wish to do this multiple times will need to type go again.
 
+
 go :: IO (String, String, String, String) 
 go = do
-      putStrLn ("Insert the 1st Image URL or filepath.")
-      firstURL <- getLine
-      putStrLn ("Insert the 2nd Image URL or filepath.")
-      secondURL <- getLine
-      putStrLn ("Insert the filter Image URL or filepath.")
-      filterURL <- getLine
-      putStrLn ("Insert the name of the combined image.")
-      combinedImageName <- getLine
-      putStrLn ("Congratulations on merging your images! To see the result go to runtime-images folder.")
-      putStrLn(" ")
-      putStrLn ("   History:")
-      putStrLn ("       firstURL = " ++ firstURL)
-      putStrLn ("       secondURL = " ++ secondURL)
-      putStrLn ("       filterURL = " ++ filterURL)
-      putStrLn ("       combinedImageName = " ++ combinedImageName)
-       putStrLn(" ")
-      return (firstURL, secondURL, filterURL, combinedImageName)
+    putStrLn ("Insert the 1st Image URL or filepath.")
+    firstName <- getLine
+    readingImage firstName
+    putStrLn ("Insert the 2nd Image URL or filepath.")
+    secondName <- getLine
+    readingImage secondName
+    putStrLn ("Insert the filter Image URL or filepath.")    
+    filterName <- getLine
+    readingImage filterName
+
+    putStrLn ("Insert the name of the combined image.")
+    combinedImageName <- getLine
+
+    putStrLn ("Congratulations on merging your images! To see the result go to runtime-images folder.")
+    putStrLn(" ")
+    putStrLn ("   History:")
+    putStrLn ("       firstName = " ++ firstName)
+    putStrLn ("       secondName = " ++ secondName)     
+    putStrLn ("       filterName = " ++ filterName)      
+    putStrLn ("       combinedImageName = " ++ combinedImageName)
+    putStrLn(" ")
+    return (firstName, secondName, filterName, combinedImageName)
       
